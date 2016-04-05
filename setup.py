a4477481d44b8349a46a029174d275be00786c26
mikedarcy <mikedarcy@users.noreply.github.com>
2016-04-05 16:40:16 -0700

    Fix dependency name.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=['ordereddict',
                       'requests',
                       'bagit==1.5.4.dev',
-                      'bagit-profiles-validator==1.0.2'],
+                      'bagit-profile==1.0.2'],
     dependency_links=[
          "http://github.com/ini-bdds/bagit-python/archive/master.zip#egg=bagit-1.5.4.dev",
          "http://github.com/ini-bdds/bagit-profiles-validator/archive/master.zip#egg=bagit-profiles-validator-1.0.2"
