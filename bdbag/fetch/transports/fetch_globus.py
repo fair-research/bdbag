874946ab6f560f4f0184a5fcf8b3341bae6d1d22
Kyle Chard <chard@uchicago.edu>
2016-05-09 20:12:48 -0700

    Updating to use the Globus SDK.

--- bdbag/fetch/transports/fetch_globus.py
+++ bdbag/fetch/transports/fetch_globus.py
@@ -6,7 +6,7 @@ import json
 import requests
 import bdbag
 import bdbag.fetch.auth.keychain as keychain
-from globusonline.transfer.api_client import TransferAPIClient, Transfer
+import globus_sdk
 
 if sys.version_info > (3,):
     from urllib.parse import urlsplit
@@ -15,18 +15,13 @@ else:
 
 logger = logging.getLogger(__name__)
 
-TOKENS = dict()
 KEYCHAIN = keychain.read_config(keychain.DEFAULT_KEYCHAIN_FILE)
 
 
 def validate_auth_config(auth):
-    if not keychain.has_auth_attr(auth, 'auth_uri'):
-        return False
     if not keychain.has_auth_attr(auth, 'auth_params'):
         return False
-    if not keychain.has_auth_attr(auth.auth_params, 'username'):
-        return False
-    if not keychain.has_auth_attr(auth.auth_params, 'password'):
+    if not keychain.has_auth_attr(auth.auth_params, 'token'):
         return False
     if not keychain.has_auth_attr(auth.auth_params, 'local_endpoint'):
         return False
@@ -36,41 +31,13 @@ def validate_auth_config(auth):
 
 def authenticate(url):
 
-    response = None
-    session = requests.session()
-
     for auth in list((entry for entry in KEYCHAIN if hasattr(entry, 'uri') and (entry.uri.lower() in url.lower()))):
-
         try:
             if not validate_auth_config(auth):
                 continue
-
-            auth_type = auth.auth_type if keychain.has_auth_attr(auth, 'auth_type', quiet=True) else 'http-basic'
-            auth_method = auth.auth_method.lower() if keychain.has_auth_attr(auth, 'auth_type', quiet=True) else 'get'
-
-            if auth_type == 'http-basic':
-                if auth.auth_params.password in TOKENS:
-                    token, endpoint = TOKENS[auth.auth_params.password]
-                    return token, endpoint
-                session.auth = (auth.auth_params.username, auth.auth_params.password)
-                if auth_method == 'post':
-                    response = session.post(auth.auth_uri, auth=session.auth)
-                elif auth_method == 'get':
-                    response = session.get(auth.auth_uri, auth=session.auth)
-                if response.status_code > 203:
-                    logger.warn('Globus authentication failed with Status Code: %s %s' %
-                                (response.status_code, response.text))
-                else:
-                    logger.info("Globus authentication successful: %s", auth.auth_uri)
-                    token = json.loads(response.text)
-                    access_token = token['access_token']
-                    TOKENS[auth.auth_params.password] = access_token, auth.auth_params.local_endpoint
-                    return access_token, auth.auth_params.local_endpoint
-            else:
-                continue
-
+            return auth.auth_params.token, auth.auth_params.local_endpoint
         except Exception as e:
-            logger.warn("Unhandled exception during Globus authentication: %s" % bdbag.get_named_exception(e))
+            logger.warn("Unhandled exception getting Globus token: %s" % bdbag.get_named_exception(e))
 
     return None, None
 
@@ -91,34 +58,35 @@ def get_file(url, output_path, token=None, dest_endpoint=None):
             logger.warn("A valid Globus access token is required to create transfers. "
                         "Check keychain.cfg for valid parameters.")
             return False
+
         if dest_endpoint is None:
             logger.warn("A valid Globus destination endpoint must be specified. "
                         "Check keychain.cfg for valid parameters.")
             return False
 
         # initialize transfer client
-        client = TransferAPIClient(None, goauth=token.strip())
+        client = globus_sdk.TransferClient(token=token)
 
         # Activate source endpoint
         logger.debug("Activating source endpoint: %s" % src_endpoint)
-        code, reason, data = client.endpoint_autoactivate(src_endpoint, if_expires_in=600)
-        logger.debug("Activation expires: %s" % data["expire_time"])
+        data = client.endpoint_autoactivate(src_endpoint, if_expires_in=600)
 
         # Activate destination endpoint
         logger.debug("Activating destination endpoint: %s" % dest_endpoint)
-        code, reason, data = client.endpoint_autoactivate(dest_endpoint, if_expires_in=600)
-        logger.debug("Activation expires: %s" % data["expire_time"])
+        data = client.endpoint_autoactivate(dest_endpoint, if_expires_in=600)
 
-        # get a unique ID for this transfer
-        code, message, data = client.transfer_submission_id()
-        submission_id = data["value"]
-
-        # start the transfer
         filename = src_path.rsplit('/', 1)[-1]
         label = "".join(("BDBag Fetch -- ", filename.replace('.', '_')))
-        transfer = Transfer(submission_id, src_endpoint, dest_endpoint, label=label)
-        transfer.add_item(src_path, dest_path)
-        code, reason, data = client.transfer(transfer)
+
+        # get a unique ID for this transfer
+        tdata = globus_sdk.TransferData(client, src_endpoint,
+                                            dest_endpoint,
+                                            label=label)
+
+        tdata.add_item(src_path, dest_path, recursive=False)
+
+        # start the transfer
+        data = client.submit_transfer(tdata)
         task_id = data["task_id"]
 
         logger.info("Globus transfer started with ID %s" % task_id)
