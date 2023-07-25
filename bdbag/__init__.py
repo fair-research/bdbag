76ea734c666d753cefee9ce3fee58fa1d9826106
mikedarcy <mdarcy@isi.edu>
2023-07-25 11:36:27 -0700

    Set version for release.

--- bdbag/__init__.py
+++ bdbag/__init__.py
@@ -26,7 +26,7 @@ from importlib_metadata import distribution, PackageNotFoundError
 
 logger = logging.getLogger(__name__)
 
-__version__ = "1.7.0dev6"
+__version__ = "1.7.0"
 __bagit_version__ = "1.8.1"
 __bagit_profile_version__ = "1.3.1"
 
